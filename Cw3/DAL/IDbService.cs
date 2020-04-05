6d7393d8eb0112413d5f26832fbd9de9154ad4b0
s17722 <s17722@pjwstk.edu.pl>
2020-04-05 23:32:57 +0200

    Init

---
+++ Cw3/DAL/IDbService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cw3.Models;
+
+namespace Cw3.DAL
+{
+    public interface IDbService
+    {
+        public IEnumerable<Student> GetStudents();
+
+        public List<Student> AddStudent(Student s);
+
+        public List<Student> RemoveStudent(int id);
+
+        public void UpdateStudent(Student s, int id);
+    }
+}
---
+++ Cw3/DAL/MockDbService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cw3.Models;
+
+namespace Cw3.DAL
+{
+    public class MockDbService : IDbService
+    {
+        private static IEnumerable<Student> _students;
+
+        static MockDbService()
+        {
+            _students = new List<Student>
+            {
+                new Student{IdStudent=1, FirstName="Jan", LastName="Kowalski"},
+                new Student{IdStudent=2, FirstName="Anna", LastName="Malewska"},
+                new Student{IdStudent=3, FirstName="Andrzej", LastName="Andrzejewicz"},
+            };
+        }
+        public IEnumerable<Student> GetStudents()
+        {
+            return _students;
+        }
+
+        public List<Student> AddStudent(Student s)
+        {
+            var List = new List<Student>(_students);
+            List.Add(s);
+            return List;
+        }
+
+        public void UpdateStudent(Student s, int id)
+        {
+            var tmpList = RemoveStudent(id);
+            AddStudent(s);
+
+        }
+
+        public List<Student> RemoveStudent(int id)
+        {
+            var tmpStudentList = GetStudents().Where(s => s.IdStudent != id).ToList();
+
+ //           var modifiedStudentList = tmpStudentList.Where(s => s.IdStudent == id).ToList();
+
+            return tmpStudentList;
+
+        }
+    }
+}
